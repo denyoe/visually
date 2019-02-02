a7632bb9b410e3e0577aa527f46de99db099c574
Marcus Ek <marcusekon@gmail.com>
2019-02-02 02:12:55 +0100

    [WIP] Media Manager

--- media_manager/models.py
+++ media_manager/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
-class media(models.Model):
-	title=models.CharField()
+class Media(models.Model):
+	title=models.CharField(max_length=200)
 	original_url=models.CharField()
 	internal_url=models.CharField()
 	m_format=models.CharField()
@@ -11,3 +11,7 @@ class media(models.Model):
 	def __str__(self):
 		return self.title + ' ( ' + self.url + ' ) ' + '[ ' + self.uploader + ' ]'
 
+
+class Text(models.Model):
+	media = models.ForeignKey(Media, on_delete=models.CASCADE)
+	text = models.TextField()
