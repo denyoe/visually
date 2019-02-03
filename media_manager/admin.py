edcdfd6ffdf0456bc90f1d46d93b25e392e706e1
Marcus Ek <marcusekon@gmail.com>
2019-02-03 08:52:25 +0100

    Update Models

--- media_manager/admin.py
+++ media_manager/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
-from . models import Media, Text
+from . models import Media, Text, Candidate, Question, Score
 
+admin.site.site_title = 'Admin'
+admin.site.site_header = 'Admin'
 
-admin.site.register(Media)
-admin.site.register(Text)
+@admin.register(Candidate, Media, Score, Question, Text)
+class MyAdmin(admin.ModelAdmin):
+	pass
+
+# admin.site.register(Candidate, MyAdmin)
--- media_manager/models.py
+++ media_manager/models.py
@@ -1,21 +1,64 @@
 from django.db import models
+from django_mysql.models import ListCharField, SetTextField
+from django.utils.translation import ugettext_lazy as _
+
+class Candidate(models.Model):
+	name 	= models.CharField(max_length=255)
+	email 	= models.EmailField(max_length=255)
+
+	def __str__(self):
+		return "{} - {}".format(self.name, self.email)
+
+
+class Question(models.Model):
+	text 	= models.TextField()
+	reference_answer = models.TextField(default=None, blank=True, null=True)
+	keywords_csv = SetTextField(
+			models.CharField(max_length=70, blank=True),
+		)
+	# keywords_csv = models.TextField()
+
+	def __str__(self):
+		return self.text
+
 
 class Media(models.Model):
+	class Meta:
+		verbose_name = _("AnswerMedia")
+		verbose_name_plural = _("AnswerMedia")
+
 	title 			= models.CharField(max_length=255)
 	original_url	= models.CharField(max_length=255)
 	internal_url	= models.CharField(max_length=255)
 	m_format		= models.CharField(max_length=20)
 	created_at		= models.DateTimeField(auto_now_add=True)
-	uploader		= models.EmailField(max_length=255) # email
+	# candidate		= models.EmailField(max_length=255)
+	candidate		= models.ForeignKey(Candidate, on_delete=models.CASCADE)
+	question		= models.ForeignKey(Question, on_delete=models.CASCADE)
 
 	def __str__(self):
-		return self.title + ' [' + self.m_format + ']' + ' ( ' + self.internal_url + ' ) ' + '[ ' + self.uploader + ' ]'
-		# return "{} posted at {}".format(
-  #           self.question_text,
-  #           self.pub_date)
+		return self.title
 
+class Score(models.Model):
+	candidate = models.ForeignKey(Candidate, on_delete=models.CASCADE)
+	question = models.ForeignKey(Question, on_delete=models.CASCADE)
+	similarity_score = models.FloatField(null = True, blank = True, default = None)
+
+	def __str__(self):
+		return "[{}] scored {} on {}".format(self.candidate.email, self.similarity_score, self.question.text)
 
 
 class Text(models.Model):
-	media 	= models.ForeignKey(Media, on_delete=models.CASCADE)
-	text 	= models.TextField()
+	class Meta:
+		verbose_name = _("AnswerText")
+		verbose_name_plural = _("AnswerText")
+
+	media 		= models.ForeignKey(Media, on_delete=models.CASCADE)
+	candidate 	= models.ForeignKey(Candidate, on_delete=models.CASCADE)
+	score 		= models.ForeignKey(Score, on_delete=models.CASCADE, null = True, blank = True, default = None)
+	text 		= models.TextField()
+
+	def __str__(self):
+		return "Answer ({}) by {} [{}]".format(self.media.title, self.candidate.name, self.candidate.email)
+
+
